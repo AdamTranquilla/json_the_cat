165995474508956351ef917366a6897a262ff8a4
Adam Tranquilla <adam.tranquilla@gmail.com>
2020-12-03 20:14:06 +0000

    retreive cat data

---
+++ breedFetcher.js
@@ -0,0 +1,17 @@
+const request = require('request');
+const [breedName] = process.argv.splice(2);
+const url = 'https://api.thecatapi.com/v1/breeds/search';
+const urlQuery = url + '?q=' + breedName;
+
+request(urlQuery, (error, response, body) => {
+
+  const data = JSON.parse(body);
+  if (!data[0]) {
+    console.log('breed is not found');
+    return;
+  }
+  if (response.status > 400) {
+    throw error;
+  }
+  console.log(data[0]);
+});

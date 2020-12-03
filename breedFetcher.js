8a86045534bd9682207f3c9fbe0f6cd4e1266229
Adam Tranquilla <adam.tranquilla@gmail.com>
2020-12-03 21:20:05 +0000

    retreive cat data w/ test

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,27 @@
 const request = require('request');
-const [breedName] = process.argv.splice(2);
-const url = 'https://api.thecatapi.com/v1/breeds/search';
-const urlQuery = url + '?q=' + breedName;
 
-request(urlQuery, (error, response, body) => {
-  const data = JSON.parse(body);
-  if (!data[0]) {
-    console.log('breed is not found');
-    return;
-  }
-  if (!response.status === 200) {
-    throw error;
-  }
-  console.log(data[0]);
-});
+const fetchBreedDescription = function(breedName, callback) {
+
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+
+  request(url, (error, response, body) => {
+
+    const data = JSON.parse(body);
+    const breed = data[0];
+
+    if (error) {
+      return callback(error, null);
+    }
+
+
+
+    if (breed) {
+      callback(null, breed.description);
+    } else {
+      callback('breed not found')
+    }
+    
+  });
+};
+
+module.exports = { fetchBreedDescription };
